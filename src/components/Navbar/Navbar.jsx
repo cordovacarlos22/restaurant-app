543be2ce0118a0c9c7242ba0e61589d1aa7f1bdf
CARLOS CORDOVA <cordovamanuel22@icloud.com>
2022-12-11 11:02:17 -0800

    setup

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Navbar.css';
+import {GiHamburgerMenu} from 'react-icons/gi'
+import MdOutline
+
+const Navbar = () => (
+  <div>
+    Navbar
+  </div>
+);
+
+export default Navbar;
