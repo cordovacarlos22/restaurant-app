10d8fda8f681d057a2eb3768c49bf53d9909c019
CARLOS CORDOVA <cordovamanuel22@icloud.com>
2022-12-17 06:55:51 -0800

    aboutus

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import './AboutUs.css';
-
+import Knife from '/Users/carloscordova/Desktop/restaurant-app/src/assets/knife.png';
+import Spoon from "/Users/carloscordova/Desktop/restaurant-app/src/assets/spoon.svg";
 const AboutUs = () => (
-
-  <div>
-    AboutUs
+  <div className="aboutusmain-container" >
+    <div className="about-us-container">
+      <h1 className="about-us-title">About us </h1>
+      <img className="about-us-spoon" src={Spoon} alt="spoon"></img> <br></br>
+      <p className="about-us-paragraph">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio quas incidunt quasi doloremque sapiente voluptatibus, id excepturi itaque modi quod. Eius dolore voluptatem ullam voluptas delectus assumenda repudiandae nobis culpa!</p>
+    </div>
+    <div className='knife-main-container'>
+      <img className="about-us-knife" src={Knife} alt="knife"></img>
+    </div>
+    <div className="our-history-container">
+      <h1 className="our-history-title">Our History</h1>
+      <img className="our-history-spoon" src={Spoon} alt="spoon"></img> <br></br>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, ipsa. Suscipit sed, sequi magnam harum pariatur a possimus, voluptates amet, aspernatur vel nostrum reiciendis. Explicabo atque ullam voluptatum molestias ad!</p>
+    </div>
   </div>
 );
 
